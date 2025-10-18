b85db56dff385419324c50df3a8bf8ae7f0ee745
deesitech <donneil.oconnor@mymona.uwi.edu>
2025-10-17 23:51:54 -0500

    Exercise 1 Completed - Layout the board

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const board = document.getElementById('board');
+    const squares = board.getElementsByTagName('div');
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].classList.add('square');
+    }
+});
+
